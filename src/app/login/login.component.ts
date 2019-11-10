73d2663b18b9c4b6d74396227a9fbc9e59a21f95
Minhaj Uddin Khan <minhajuddin.khan@tenpearls.com>
2019-11-10 20:25:12 +0500

    login html written

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,20 +1,67 @@
 import { Component, OnInit } from "@angular/core";
-import { FormControl } from "@angular/forms";
+import { FormGroup, FormControl } from "@angular/forms";
+import { MatSnackBar } from "@angular/material/snack-bar";
 import { AuthService } from "./login.service";
+
+import {
+  trigger,
+  state,
+  style,
+  animate,
+  transition
+} from "@angular/animations";
+import { HttpErrorResponse } from "@angular/common/http";
+
 @Component({
   selector: "app-login",
+  animations: [
+    // Each unique animation requires its own trigger. The first argument of the trigger function is the name
+    trigger("rotatedState", [
+      state("default", style({ transform: "rotate(0)" })),
+      state("rotated", style({ transform: "rotate(-360deg)" })),
+      // transition("rotated => default", animate("1000ms ease-out"))
+      transition("default => rotated", animate("1000ms ease-in"))
+    ])
+  ],
   templateUrl: "./login.component.html",
   styleUrls: ["./login.component.scss"]
 })
 export class LoginComponent {
-  username = new FormControl("");
-  constructor(private authService: AuthService) {}
+  state = "default";
+  loginForm = new FormGroup({
+    username: new FormControl("")
+  });
 
-  ngOnInit() {}
+  constructor(private authService: AuthService, private snackBar: MatSnackBar) {
+    setInterval(() => {
+      this.rotate();
+    }, 3000);
+  }
 
-  onLoginClick(username: string) {
-    this.authService.authenticate(username).subscribe(resp => {
-      console.log(resp);
-    });
+  rotate() {
+    this.state = this.state === "default" ? "rotated" : "default";
+  }
+
+  onLoginClick() {
+    const { username } = this.loginForm.value;
+    this.authService.authenticate(username).subscribe(
+      (resp: any) => {
+        localStorage.setItem("token", resp.token);
+      },
+      (error: HttpErrorResponse) => {
+        if (error.status === 401) {
+          this.snackBar.open(
+            "Sorry, Invalid credentials buddy.",
+            "Can't Login",
+            {
+              duration: 3 * 1000
+            }
+          );
+          return;
+        }
+
+        this.snackBar.open("something went wrong.");
+      }
+    );
   }
 }
--- src/app/login/login.module.ts
+++ src/app/login/login.module.ts
@@ -4,9 +4,21 @@ import { LoginComponent } from "./login.component";
 import { ReactiveFormsModule } from "@angular/forms";
 import { AuthService } from "./login.service";
 import { HttpClientModule } from "@angular/common/http";
+import { MatFormFieldModule } from "@angular/material/form-field";
+import { MatInputModule } from "@angular/material/input";
+import { MatButtonModule } from "@angular/material/button";
+import { MatSnackBarModule } from "@angular/material/snack-bar";
+
 @NgModule({
   declarations: [LoginComponent],
-  imports: [CommonModule, ReactiveFormsModule, HttpClientModule],
+  imports: [
+    CommonModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule
+  ],
   providers: [AuthService]
 })
 export class LoginModule {}
