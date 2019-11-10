73c72da0d9c084e45d02c5e84bcd0d12a8ad154e
Minhaj Uddin Khan <minhajuddin.khan@tenpearls.com>
2019-11-10 20:25:12 +0500

    login html written

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,28 @@
 import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
-import { ReactiveFormsModule, FormsModule } from "@angular/forms";
+import { ReactiveFormsModule } from "@angular/forms";
 
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
 import { CardComponent } from "./card/card.component";
 import { LoginModule } from "./login/login.module";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { MatFormFieldModule } from "@angular/material/form-field";
+import { MatSliderModule } from "@angular/material/slider";
+import { MatSnackBarModule } from "@angular/material/snack-bar";
+
 @NgModule({
   declarations: [AppComponent, CardComponent],
-  imports: [BrowserModule, AppRoutingModule, LoginModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    LoginModule,
+    BrowserAnimationsModule,
+    MatSliderModule,
+    MatFormFieldModule,
+    ReactiveFormsModule,
+    MatSnackBarModule
+  ],
   providers: [],
   bootstrap: [AppComponent]
 })
