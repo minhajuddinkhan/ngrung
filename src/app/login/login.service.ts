2564ed3b41281b476def2947a6eee8a906df897c
Minhaj Uddin Khan <minhajuddin.khan@tenpearls.com>
2019-11-16 21:08:51 +0500

    Integrate user info api

--- src/app/login/login.service.ts
+++ src/app/login/login.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from "@angular/core";
-import { HttpClient } from "@angular/common/http";
+import { HttpClient, HttpHeaders } from "@angular/common/http";
 import { environment } from "../../environments/environment";
 
 @Injectable({
@@ -12,5 +12,16 @@ export class AuthService {
     return this.httpClient.post(`${environment.rootURL}/api/v1/auth`, {
       username
     });
+  }
+
+  whoami() {
+    const httpOptions = {
+      headers: new HttpHeaders({
+        "Content-Type": "application/json",
+        token: localStorage.getItem("token")
+      })
+    };
+
+    return this.httpClient.get(`${environment.rootURL}/api/v1/me`, httpOptions);
   }
 }
